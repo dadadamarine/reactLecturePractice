c87d3d0161bdb51ad9c5643f2cf67efdbc3cfcc8
dadadamarine <dadadamarine@naver.com>
2018-10-05 15:55:07 +0900

    css 살짝 적용

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -18,7 +18,7 @@ class SearchBar extends Component {
 
     render () {
         return (
-            <div>
+            <div className='search-bar' >
                 <input 
                     value={this.state.term}
                     onChange={e => this.setState({term: e.target.value})} />
